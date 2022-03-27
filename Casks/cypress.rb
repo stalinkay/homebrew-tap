5388c401b51a12875a434c1dfca2ddfe035909a9
Stalin Kay <github.com@stalinkay.work>
2022-03-27 03:28:43 +0200

    Create cypress.rb

---
+++ Casks/cypress.rb
@@ -0,0 +1,11 @@
+cask "cypress" do
+  version "9.5.2"
+  sha256 "160a470d9259c91f7f4155b7b1da6504bca3cf153a15b8b2c0e06adee787b375"
+
+  url "https://cdn.cypress.io/desktop/#{version}/darwin-x64/cypress.zip"
+  name "cypress"
+  desc ""
+  homepage ""
+
+  app ""
+end
