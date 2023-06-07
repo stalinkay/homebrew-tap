f7fe5eafd8dfe5519ae09db4a309fad8f2028b22
Stalin Kay <github.com@stalinkay.work>
2023-06-07 15:00:17 +0200

    Update capcut.rb

--- Casks/capcut.rb
+++ Casks/capcut.rb
@@ -1,8 +1,8 @@
 cask "capcut" do
-  version "506"
+  version "2_2_0_506"
   sha256 "4f9b94e371162b180cfe79d0e3b9488ed46ba63da5e662935bd6dee13f6d8ab1"
 
-  url "https://lf16-capcut.faceulv.com/obj/capcutpc-packages-us/packages/CapCut$"
+  url "https://lf16-capcut.faceulv.com/obj/capcutpc-packages-us/packages/CapCut_#{version}_capcutpc_0_creatortool.dmg"
   name "CapCut"
   desc "Produce stunning videos easily with auto captions, text-to-speech, effects, filters, text features, transitions, and many more."
   homepage "https://www.capcut.com"
