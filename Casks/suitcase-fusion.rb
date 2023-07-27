2c69144526676ae48582e5bcbf2285b09d864654
Stalin Kay <github.com@stalinkay.work>
2023-07-27 21:01:33 +0200

    Update suitcase-fusion.rb

--- Casks/suitcase-fusion.rb
+++ Casks/suitcase-fusion.rb
@@ -3,9 +3,13 @@ cask 'suitcase-fusion' do
   sha256 '7a1df59d333962cc3db4fd61331ab7d33733caeaeb21c96e2daa2361f6732db5'
 
   url "https://bin.extensis.com/SuitcaseFusion#{version.before_comma}-M-#{version.after_comma.dots_to_hyphens}.dmg"
-  appcast "https://sparkle.extensis.com/u/ST/EN/suitcase#{version.after_comma.major}en.xml"
   name 'Extensis Suitcase Fusion'
   homepage 'https://www.extensis.com/'
+
+  livecheck do
+    url "https://sparkle.extensis.com/u/ST/EN/suitcase#{version.after_comma.major}en.xml"
+    strategy :sparkle
+  end
 
   app 'Suitcase Fusion.app'
 end
