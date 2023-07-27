4260f7e1ffc694be691c2bb103ee62496e638fbb
Stalin Kay <github.com@stalinkay.work>
2023-07-27 20:53:59 +0200

    Update sshpass.rb

--- Formula/sshpass.rb
+++ Formula/sshpass.rb
@@ -1,20 +1,25 @@
 require 'formula'
 
 class Sshpass < Formula
+  homepage 'https://sourceforge.net/projects/sshpass'
   url "https://downloads.sourceforge.net/project/sshpass/sshpass/1.09/sshpass-1.09.tar.gz"
   sha256 "71746e5e057ffe9b00b44ac40453bf47091930cba96bbea8dc48717dedc49fb7"
-  homepage 'https://sourceforge.net/projects/sshpass'
 
   depends_on 'gnu-sed'
+
+  livecheck do
+    url :stable
+    regex(%r{url=.*?/sshpass[._-]v?(\d+(?:\.\d+)+)\.t}i)
+  end
 
   def install
     system "./configure", "--disable-debug",
                           "--disable-dependency-tracking",
                           "--prefix=#{prefix}"
-    system "make install"
+    system "make", "install"
   end
 
-  def test
-    system "sshpass"
+  test do
+    system "#{bin}/sshpass"
   end
 end
