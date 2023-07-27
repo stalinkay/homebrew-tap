30eedda15e80dcb7269fc96a3c6b4c25706c6828
Stalin Kay <github.com@stalinkay.work>
2023-07-27 15:22:50 +0200

    Update macports.rb

--- Casks/macports.rb
+++ Casks/macports.rb
@@ -3,17 +3,26 @@ cask "macports" do
   sha256 "577512628a4b9237b3eccd0e18af28e06855f5d55bd71957c37a9c7c362de5f3"
 
   url "https://github.com/macports/macports-base/releases/download/v#{version}/MacPorts-#{version}-13-Ventura.pkg"
-  name "MacPorts"
+  name "macports"
   desc "Open-source community initiative to design an easy-to-use system for compiling, installing, and upgrading either command-line, X11 or Aqua based open-source software on the macOS operating system"
   homepage "https://www.macports.org/"
 
   livecheck do
-    url :homepage
-    regex(/MacPorts (\d+(?:\.\d+)*).*/)
+    url :stable
+    regex(/^v?(\d+(?:\.\d+)+)$/i)
+    strategy :github_releases do |json, regex|
+      json.map do |release|
+        next if release["prerelease"] || release["draft"]
+
+        match = release["tag_name"].match(regex)
+        next if match.blank?
+
+        match[1]
+      end
+    end
   end
 
   pkg "MacPorts-#{version}-13-Ventura.pkg"
 
-  uninstall pkgutil: "org.macports.MacPorts",
-            delete:  "/Applications/MacPorts"
+  uninstall pkgutil: "org.macports.MacPorts"
 end
