2418035bfd10138f3b9c228bd3f12a4ca59a9ba3
Stalin Kay <github.com@stalinkay.work>
2023-07-27 18:26:00 +0200

    Update julia.rb

--- Casks/julia.rb
+++ Casks/julia.rb
@@ -1,15 +1,29 @@
 cask "julia" do
   version "1.5.3"
-  sha256 "802cdb67748bbd8a943604981d4f2a2c4c98899f5bbad0aef8a7ea8bc9b31abd"
+  sha256 "802cdb67748bbd8a943604981d4f2aef8a7ea8bc9b31abd"
 
   url "https://julialang-s3.julialang.org/bin/mac/x64/#{version.major_minor}/julia-#{version}-mac64.dmg"
-  appcast "https://github.com/JuliaLang/julia/releases.atom"
   name "Julia"
   desc "Programming language for technical computing"
   homepage "https://julialang.org/"
 
   app "Julia-#{version.major_minor}.app"
   binary "#{appdir}/Julia-#{version.major_minor}.app/Contents/Resources/julia/bin/julia"
+
+  livecheck do
+    url :stable
+    regex(/^v?(\d+(?:\.\d+)+)$/i)
+    strategy :github_releases do |json, regex|
+      json.map do |release|
+        next if release["prerelease"] || release["draft"]
+
+        match = release["tag_name"].match(regex)
+        next if match.blank?
+
+        match[1]
+      end
+    end
+  end
 
   zap trash: "~/.julia"
 end
