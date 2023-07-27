8464e79e3b2e786f55b54a2d30d1511d4dc0c717
Stalin Kay <github.com@stalinkay.work>
2023-07-27 17:58:57 +0200

    Update markup-hero.rb

--- Casks/markup-hero.rb
+++ Casks/markup-hero.rb
@@ -8,7 +8,7 @@ cask "markup-hero" do
   homepage "https://markuphero.com"
 
   livecheck do
-    url "#{homepage}/download"
+    url "#{:homepage}/download"
     regex(/href=.*?MarkupHero[._-]v?(\d+(?:\.\d+)+)\.dmg/i)
   end
 
