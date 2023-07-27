9c3a9b77e615421044d582adf3b4faa9312a53b7
Stalin Kay <github.com@stalinkay.work>
2023-07-27 17:52:16 +0200

    Update markup-hero.rb

--- Casks/markup-hero.rb
+++ Casks/markup-hero.rb
@@ -3,9 +3,14 @@ cask "markup-hero" do
   sha256 "dc08ba340df508dd8326c36a87c7ddea80df71c3a009fddbbb44f158226d01b5"
 
   url "https://storage.googleapis.com/markuphero-dist/MarkupHero-#{version}.dmg"
-  name "markup-hero"
+  name "Markup Hero"
   desc "Markup Hero is the screenshot and annotation tool made for the web. Works on Mac, Windows, Linux and Chrome."
   homepage "https://markuphero.com"
 
-  app "Markup Hero.app"
+  livecheck do
+    url "#{homepage}/download"
+    regex(/href=.*?MarkupHero[._-]v?(\d+(?:\.\d+)+)\.dmg/i)
+  end
+
+  app "MarkupHero.app"
 end
