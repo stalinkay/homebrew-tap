462077d1aa816a147235d09440ebfab7f7762654
Stalin Kay <github.com@stalinkay.work>
2023-11-16 04:43:10 +0200

    Update aide.rb

--- Casks/aide.rb
+++ Casks/aide.rb
@@ -1,11 +1,17 @@
 cask "aide" do
   version "1.84.2.23314"
-  sha256 "1c540bfe2573187b63b1dd846c0fed387ada309e3e309099742d9f4189805164"
+  
+  if Hardware::CPU.intel?
+    sha256 "1c540bfe2573187b63b1dd846c0fed387ada309e3e309099742d9f4189805164" # Replace with the actual SHA256 for the Intel version
+    url "https://github.com/codestoryai/binaries/releases/download/#{version}/Aide-darwin-x64-#{version}.zip"
+  else
+    sha256 "eb7aa3d19d5867ea0da8e470d4064eb24fcf1faf464df30ca2917ab7397a80d1" # Replace with the actual SHA256 for the ARM64 version
+    url "https://github.com/codestoryai/binaries/releases/download/#{version}/Aide-darwin-arm64-#{version}.zip"
+  end
 
-  url "https://github.com/codestoryai/binaries/releases/download/#{version}/Aide-darwin-x64-#{version}.zip"
   name "Aide"
   desc "Instruct AI agents to build your software"
   homepage "https://codestory.ai"
 
-  app "Aide.app" # Specify the path to the app within the downloaded zip file
+  app "Aide.app"
 end
